bfdfe5d3c996231d2d16baccc1c3c8ce771ff638
LABOREMUS\patricia <patricia@emata.io>
2022-06-08 17:05:23 +0300

    Added json server and router

--- task-tracker/src/components/Header.vue
+++ task-tracker/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button
+    <Button v-show="homePage"
       @btn-click="$emit('toggle-add-task')"
       :text="showAddTask ? 'Close': 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -25,7 +25,17 @@ export default {
   components: {
     Button,
   },
-};
+  computed: {
+    homePage(){
+      if(this.$route.path === '/'){
+        return true
+      } else {
+        return false
+      }
+      }
+    }
+  }
+
 </script>
 
 <style scoped>
